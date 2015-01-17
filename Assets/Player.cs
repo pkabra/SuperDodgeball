9fa4708bae6c0722f9fdd68592aa4d99e8f2220d
pkabra <pratik@kabra.com>
2015-01-17 00:02:53 -0500

    init

---
+++ Assets/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Mathf.Round (Input.GetAxisRaw ("Vertical"));
+
+		// Movement
+		Vector3 pos1 = this.transform.position;
+		pos1.x += h * 0.1f;
+		pos1.y += v * 0.1f;
+		this.transform.position = pos1;
+	}
+}
