3bf194bc736c4c147b45523e92167c5ef4b14ce9
Steve Roudebush <roudy@umich.edu>
2015-01-19 19:19:45 -0500

    Ball Bounce
    The ball now bounces off the ground and vertical surfaces that it hits.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public enum BallState { rest, free, held, pass, thrown };
-public enum Trajectory { none, low, mid, high };
+public enum Trajectory { none, low, mid, EastWestHigh, NorthSouthHigh };
 
 public class Ball : MonoBehaviour {
 	public Vector3 vel = Vector3.zero;
-	public float passSpeedMult = 5f;
+	public float passSpeedMult = 3f;
 	public float throwSpeedMult = 5f;
 	public float catchRadius = 0.5f;
 	public float YCompOfZ = 0.5f; // The Y component of the Height axis, NOT the worldspace Z
@@ -26,6 +26,7 @@ public class Ball : MonoBehaviour {
 	private float heightVel = 0.0f; // velocity along the height axis, used for faking gravity
 	private float gravity = -0.07f;
 	private float bounciness = 0.5f;
+	private Vector3 prevPos = Vector3.zero;
 	
 	
 	//private GameObject passTarg = null;
@@ -37,6 +38,8 @@ public class Ball : MonoBehaviour {
 	
 	// Once per frame
 	void FixedUpdate () {
+		// Save prev position info
+		prevPos = transform.position;
 		
 		// State specific things
 		if(state == BallState.pass){
@@ -49,6 +52,7 @@ public class Ball : MonoBehaviour {
 			if(vel.magnitude < 0.02f){
 				vel = Vector3.zero;
 			}
+			this.transform.position += vel * Time.fixedDeltaTime;
 		} else if(state == BallState.rest){
 			
 		}
@@ -59,75 +63,43 @@ public class Ball : MonoBehaviour {
 		
 	}
 
-	void BounceLogic(){
 
-	}
-
-	void FreeBallinLogic(){
-		// Put ball at rest if it is moving super slow
-		if(height < 0.01f && (heightVel < 0.01f && heightVel > -0.01f)){
-			state = BallState.rest;
-			height = 0f;
-			heightVel = 0f;
-		} else {
-			heightVel += gravity;
-			height += heightVel;
-			if(height < 0f){
-				height = 0f;
-				heightVel = -heightVel * bounciness;
-			}
-		}
-	}
-	
-	void PassTrajectoryLogic(){
-		
-		float distFromOrigin = (transform.position - passOrigin).magnitude;
-		float halfDist = totalTrajDist / 2f;
-		float distFromMidpoint = distFromOrigin - halfDist;
-		float halfDistSqrd = Mathf.Pow(halfDist, 2f);
-		float percentOfMax = 1f - (Mathf.Pow (distFromMidpoint,2f) / halfDistSqrd);
-		float maxHeightMult = 0.06f * halfDistSqrd;
-		
-		if(trajectory == Trajectory.high){
-			maxHeight = 14.0f * maxHeightMult;
-			height = maxHeight * percentOfMax;
-		} else if(trajectory == Trajectory.mid){
-			maxHeight = 9.0f * maxHeightMult;
-			height = maxHeight * percentOfMax;
-		} else if(trajectory == Trajectory.low){
-			maxHeight = 3.0f * maxHeightMult;
-			height = maxHeight * percentOfMax;
-		} else {
-			print ("Error in PassTrajectoryLogic()");
-		}
-	}
 	
 	void OnTriggerEnter(Collider other){
 		if(state == BallState.held){ // Do nothing if ball held
 			return;
 		}
-		
-		if(other.gameObject.layer == 9){ // If other is on the 'Players' layer
-			// get Player component if one exists
-			Player pOther = other.GetComponent<Player>();
-			if(pOther.GetInstanceID() == catcher.GetInstanceID()){
-				StateHeld(catcher);
-			}
-			
-			foreach(Player p in GameEngine.team1) {
-				if (pOther.GetInstanceID() == p.GetInstanceID()) {
-					if (p.aState.state == ActionStates.catching) {
-						// Caught
-					} else {
-						
+
+		int othersLayer = other.gameObject.layer; 
+		Player pOther = other.GetComponent<Player>(); // maybe null if the 'other' is not a player
+
+		if(state == BallState.pass){
+			if(othersLayer == 9){ // If other is on the 'Players' layer
+				// get Player component if one exists
+				if(pOther.GetInstanceID() == catcher.GetInstanceID()){
+					StateHeld(catcher);
+				}
+				
+				foreach(Player p in GameEngine.team1) {
+					if (pOther.GetInstanceID() == p.GetInstanceID()) {
+						if (p.aState.state == ActionStates.catching) {
+							// Caught
+						} else {
+							
+						}
 					}
 				}
+			} else if(othersLayer == 11){
+				VerticalSurfaceBounce(other);
 			}
-		}else if(other.gameObject.layer == 11){ // If other is on the 'OutfieldBoundary' layer
-			//TEMP
-			vel = Vector3.zero;
-			state = BallState.free;
+		} else if (state == BallState.thrown){
+			VerticalSurfaceBounce(other);
 		}
+//		else if(other.gameObject.layer == 11){ // If other is on the 'OutfieldBoundary' layer
+//			//TEMP
+//			vel = Vector3.zero;
+//			state = BallState.free;
+//		}
 	}
 	
 	void OnTriggerStay(Collider other){
@@ -187,7 +159,7 @@ public class Ball : MonoBehaviour {
 		state = BallState.pass;
 		
 		//Set trajectory calculation info
-		trajectory = Trajectory.high;
+		trajectory = Trajectory.NorthSouthHigh;
 		Vector3 originTemp = this.holder.transform.position;
 		passOrigin = originTemp;
 		totalTrajDist = toTarg.magnitude;
@@ -219,6 +191,64 @@ public class Ball : MonoBehaviour {
 		// set throwing player to 'throwing' state then remove holder
 		holder.StateThrowing();
 		holder = null;
+	}
+
+	void VerticalSurfaceBounce(Collider other){
+		//Set up a raycast
+		Vector3 dir = vel.normalized;
+		Ray ray = new Ray(prevPos, dir);
+		RaycastHit hit;
+		other.Raycast(ray, out hit, 20.0f);
+		Vector3 norm = hit.normal;
+		
+		// Bounce off the surface
+		vel = Vector3.Reflect(vel, norm) * bounciness;
+		state = BallState.free;
+	}
+	
+	void FreeBallinLogic(){
+		// Put ball at rest if it is moving super slow
+		if(height < 0.01f && (heightVel < 0.01f && heightVel > -0.01f)){
+			state = BallState.rest;
+			height = 0f;
+			heightVel = 0f;
+		} else {
+			heightVel += gravity;
+			height += heightVel;
+			if(height < 0f){
+				// Ball has hit the ground
+				height = 0f;
+				heightVel = -heightVel * bounciness;
+				vel *= bounciness;
+			}
+		}
+	}
+	
+	void PassTrajectoryLogic(){
+		
+		float distFromOrigin = (transform.position - passOrigin).magnitude;
+		float halfDist = totalTrajDist / 2f;
+		float distFromMidpoint = distFromOrigin - halfDist;
+		float halfDistSqrd = Mathf.Pow(halfDist, 2f);
+		float percentOfMax = 1f - (Mathf.Pow (distFromMidpoint,2f) / halfDistSqrd);
+		float multOffset = 5.0f;
+		float maxHeightMult = 0.06f * (halfDistSqrd + multOffset);
+		
+		if(trajectory == Trajectory.EastWestHigh){
+			maxHeight = 14.0f * maxHeightMult;
+			height = maxHeight * percentOfMax;
+		} else if(trajectory == Trajectory.NorthSouthHigh){
+			maxHeight = 24.0f * maxHeightMult;
+			height = maxHeight * percentOfMax;
+		} else if(trajectory == Trajectory.mid){
+			maxHeight = 9.0f * maxHeightMult;
+			height = maxHeight * percentOfMax;
+		} else if(trajectory == Trajectory.low){
+			maxHeight = 3.0f * maxHeightMult;
+			height = maxHeight * percentOfMax;
+		} else {
+			print ("Error in PassTrajectoryLogic()");
+		}
 	}
 	
 }
